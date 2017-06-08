d49e66e5c97f4afade12ba15cb9196fffa845cfc
Alex Lindgren <alindgren@flightpath.com>
2017-06-08 11:26:00 -0400

    get unit test working (binding redirect config)

--- Fundraise.Core.Tests/CampaignTests.cs
+++ Fundraise.Core.Tests/CampaignTests.cs
@@ -1,14 +1,36 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fundraise.Core.Services;
+using Fundraise.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Fundraise.Core.Tests
 {
     [TestClass]
     public class CampaignTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private ICampaignRepository _campaignRepository;
+
+        public CampaignTests()
         {
+            Init();
         }
+
+        private void Init()
+        {
+            var builder = new DbContextOptionsBuilder<FundraiseContext>()
+            .UseInMemoryDatabase();
+
+            var context = new FundraiseContext(builder.Options);
+            _campaignRepository = new CampaignRepository(context);
+        }
+
+        [TestMethod]
+        public void TestMethod()
+        {
+            Assert.IsTrue(true);
+        }
+
+
     }
 }
