98ced237a942708124429888a9ad6e41ce381494
Alex Lindgren <alindgren@flightpath.com>
2017-07-04 17:39:51 -0400

    add SimpleInjector

--- Fundraise.MvcExample/Global.asax.cs
+++ Fundraise.MvcExample/Global.asax.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Fundraise.Core.Entities;
+using Fundraise.Core.Services;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +15,22 @@ namespace Fundraise.MvcExample
     {
         protected void Application_Start()
         {
+            //// 1. Create a new Simple Injector container
+            //var container = new Container();
+
+            //// 2. Configure the container (register)
+            //// See below for more configuration examples
+            //container.Register<FundraiseContext>(() =>
+            //{
+            //    var optionsBuilder = new DbContextOptionsBuilder<FundraiseContext>();
+            //    optionsBuilder.UseSqlServer(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+            //    return new FundraiseContext(optionsBuilder.Options);
+            //});
+            //container.Register<ICampaignRepository, CampaignRepository>(Lifestyle.Singleton);
+
+            //// 3. Optionally verify the container's configuration.
+            //container.Verify();
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
