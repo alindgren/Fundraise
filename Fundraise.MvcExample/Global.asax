6905440c9d7a6acfc0d80ef6efbc6ac7989c40a3
Alex Lindgren <alindgren@flightpath.com>
2017-07-04 17:10:43 -0400

    add MVC project

---
+++ Fundraise.MvcExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fundraise.MvcExample.MvcApplication" Language="C#" %>
---
+++ Fundraise.MvcExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fundraise.MvcExample.Startup))]
+namespace Fundraise.MvcExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
