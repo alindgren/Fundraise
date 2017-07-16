854246b9122516028e11248f6c39410616ee0d04
Alex Lindgren <alindgren@flightpath.com>
2017-07-15 22:12:14 -0400

    MVC Example edit campaign form WIP

---
+++ Fundraise.MvcExample/Models/CampaignFormViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Fundraise.MvcExample.Models
+{
+    public class CampaignFormViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        public string DefaultCurrencyCode { get; set; }
+
+        [MaxLength(512)]
+        public string Description { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime EndDate { get; internal set; }
+    }
+}
