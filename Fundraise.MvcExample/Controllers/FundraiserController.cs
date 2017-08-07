0e4180880c6bd6a314beb7bff2d598711292851e
Alex Lindgren <alindgren@flightpath.com>
2017-08-07 13:05:22 -0400

    Fundraiser detail page (WIP)

---
+++ Fundraise.MvcExample/Controllers/FundraiserController.cs
@@ -0,0 +1,41 @@
+﻿using Fundraise.Core.Entities;
+using Fundraise.Core.Services;
+using Fundraise.MvcExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fundraise.MvcExample.Controllers
+{
+    public class FundraiserController : Controller
+    {
+
+        private ICampaignRepository _campaignRepository;
+        private FundraiserRepository _fundraiserRepository;
+
+        public FundraiserController(CampaignRepository campaignRepository, FundraiserRepository fundraiserRepository)
+        {
+            _campaignRepository = campaignRepository;
+            _fundraiserRepository = fundraiserRepository;
+        }
+
+        // GET: Fundraiser
+        public ActionResult Index(Guid? id)
+        {
+            if (!id.HasValue)
+            {
+                return View();
+            }
+
+            var fundraiser = _fundraiserRepository.FindById(id.Value);
+            if (fundraiser == null)
+                return HttpNotFound();
+
+            var fundraiserViewModel = AutoMapper.Mapper.Map<Fundraiser, FundraiserFormViewModel>(fundraiser);
+
+            return View("Detail", fundraiserViewModel);
+        }
+    }
+}
