fb72dd1454e342ea673e2d9eac6952204d133fe4
Alex Lindgren <alindgren@flightpath.com>
2017-08-10 19:08:07 -0400

    mvc example - start Profile page

---
+++ Fundraise.MvcExample/Controllers/ProfileController.cs
@@ -0,0 +1,38 @@
+﻿using Fundraise.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
+namespace Fundraise.MvcExample.Controllers
+{
+    public class ProfileController : Controller
+    {
+
+
+        private ICampaignRepository _campaignRepository;
+        private IFundraiserRepository _fundraiserRepository;
+        private IDonationRepository _donationRepository;
+
+        public ProfileController(CampaignRepository campaignRepository, FundraiserRepository fundraiserRepository, IDonationRepository donationRepository)
+        {
+            _campaignRepository = campaignRepository;
+            _fundraiserRepository = fundraiserRepository;
+            _donationRepository = donationRepository;
+        }
+
+        // GET: Profile
+        public ActionResult Index()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return Redirect("/");
+            }
+
+            return View();
+            
+        }
+    }
+}
